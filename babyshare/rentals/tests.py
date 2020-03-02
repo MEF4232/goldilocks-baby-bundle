02454df6a83c5f67bba8f34e8049c13895eff693
MEF4232 <mollyelizfeld@gmail.com>
2020-03-01 19:16:09 -0500

    add basic unittest

--- babyshare/rentals/tests.py
+++ babyshare/rentals/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Parent
+
+class ParentModelTests(TestCase):
+	
+	def test_unique_email_address(self):
+		""" Each parent must have a unique email address """
+		p1 = Parent(first_name = 'Joe', last_name = 'Smith', email_address = 'test@gmail.com')
+		
+		try:
+			p2 = Parent(first_name = 'Jane', last_name = 'Doe', email_address = 'test@gmail.com')
+		except:
+			# https://docs.python.org/3/library/unittest.mock.html
+			# https://stackoverflow.com/questions/27813027/django-python-unittesting-how-to-force-exception-of-try-except-block
+			pass
